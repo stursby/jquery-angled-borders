29c63ed0391d2b401729ecda0f2bfa3514d0e2bf
Charlie Hield <charles.hield@gmail.com>
2014-10-15 19:16:44 -0500

    Basic functionality working

--- dist/jquery-angled-border.js
+++ dist/jquery-angled-border.js
@@ -5,34 +5,77 @@
  * @license MIT
  * @author Charlie Hield
  */
-(function($) {
+;(function($) {
 
     'use strict';
 
-    $.fn.angledBorder = function(opts) {
+    $.fn.angledBorder = function (options) {
 
-        // default configuration
-        var config = $.extend({}, {
-            opt1: null
-        }, opts);
+        // defaults
+        var defaults = {
 
-        // main function
-        function DoSomething(e) {
-        }
+            height: 30, // in px
+            color: 'inherit', // hex value
+            direction: 'LTR', // or RTL
+            position: 'bottom' // or top
 
-        // initialize every element
-        this.each(function() {
-            DoSomething($(this));
+        }, settings = $.extend({}, defaults, options, $(this).data());
+
+        this.each(function () {
+
+            // cache $(this) var
+            var el = $(this);
+
+            // does element have position relative or absolute?
+            if (el.css('position') === 'static') {
+                el.css('position', 'relative');
+            }
+
+            // attempt to auto match border color to parent background color
+            el.css({ borderColor: el.css('background-color')});
+
+            // create border element
+            var border = $('<span class=""/>');
+
+            // base CSS
+            var properties = {
+                "content": "",
+                "position": "absolute",
+                "display": "block",
+                "width": "100%",
+                "background": "transparent",
+                "border-width": "1000px",
+                "height": settings.height
+            };
+
+            // change direction to CSS value
+            var direction;
+
+            switch(settings.direction.toLowerCase()) {
+                case 'ltr':
+                    direction = 'left';
+                    break;
+                case 'rtl':
+                    direction = 'right';
+                    break;
+            }
+
+            // set dynamic properties
+            properties['border-' + direction + '-color'] = settings.color;
+            properties['border-' + direction + '-style'] = 'solid';
+            properties['border-' + settings.position] = settings.height + "px solid transparent";
+            properties['' + settings.position] = -settings.height;
+
+            // set all the properties
+            border.css(properties);
+
+            // append styled border
+            el.append(border);
+
         });
 
-        return this;
+    return this;
+
     };
-
-    // start
-    $(function() {
-        $("#select").PlugInName();
-    });
-
-
 
 })(jQuery);
